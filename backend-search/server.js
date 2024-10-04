4ebb7acd9c98d7336a9127fb97bbad5b8de1aba2
clentebanks <clentebanks0@gmail.com>
2024-10-03 20:23:31 -0600

    gt

--- backend-search/server.js
+++ backend-search/server.js
@@ -7,19 +7,14 @@ app.use(cors());
 app.use(express.json());
 
 // Configurar la conexión con PostgreSQL
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'diccionario',
-//   password: 'test',
-//   port: 5432,
-// });
 const pool = new Pool({
-  connectionString: 'postgresql://interpreter_dictionary_kg3n_user:yTi83urcoHnOF98egDf0BpoRsrU5xizA@dpg-crvibu1u0jms73dogu2g-a.oregon-postgres.render.com/interpreter_dictionary_kg3n',  // Usamos la variable de entorno DATABASE_URL para la conexión
-  ssl: {
-    rejectUnauthorized: false  // Render usa SSL para asegurar las conexiones
-  }
+  user: 'postgres',
+  host: 'localhost',
+  database: 'diccionario',
+  password: 'test',
+  port: 5432,
 });
+
 // Ruta para buscar palabras
 app.get('/buscar', async (req, res) => {
   const { query } = req.query;
