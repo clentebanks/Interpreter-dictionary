5775c13b242854aaac9d99e9d5014b170eaaa3bd
clentebanks <clentebanks0@gmail.com>
2024-10-25 23:48:45 -0600

    fgtr

--- backend-search/server.js
+++ backend-search/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // PostgreSQL client setup
 const pool = new Pool({
@@ -10,19 +10,22 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 });
-// Ruta para buscar palabras
-app.get('/buscar', async (req, res) => {
-  const { query } = req.query;
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Search route
+app.get('/buscar', async (req, res) => {
+  console.log('Request received at /buscar');
+  const { palabra } = req.query;
   try {
-    const result = await pool.query(
-      'SELECT * FROM palabras WHERE palabra ILIKE $1 OR acronimo IS TRUE AND palabra ILIKE $1',
-      [`%${query}%`]
-    );
+    const result = await pool.query('SELECT * FROM palabras WHERE palabra = $1', [palabra]);
     res.json(result.rows);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Error en el servidor' });
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
