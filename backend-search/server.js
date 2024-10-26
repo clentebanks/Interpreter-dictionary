e47db301f20121fe387f75ad056a8176d77bf61c
clentebanks <clentebanks0@gmail.com>
2024-10-26 10:13:45 -0600

    back

--- backend-search/server.js
+++ backend-search/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const { Pool } = require('pg');
-const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-// PostgreSQL client setup using DATABASE_URL environment variable
+// PostgreSQL client setup
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -12,25 +11,25 @@ const pool = new Pool({
   }
 });
 
-// Middleware to serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Route to fetch data from the database
-app.get('/data', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM palabras');
-    res.json(result.rows);
-  } catch (err) {
-    console.error('Error fetching data:', err);
-    res.status(500).json({ error: 'Internal Server Error', details: err.message });
-  }
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-// Route to serve the HTML file
-app.get('/popup', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'popup.html'));
+// Search route
+app.get('/buscar', async (req, res) => {
+  console.log('Request received at /buscar');
+  const { palabra } = req.query;
+  try {
+    const result = await pool.query('SELECT * FROM palabras WHERE palabra ILIKE $1 OR acronimo IS TRUE AND palabra ILIKE $1', [palabra]);
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Database query error:', err);
+    res.status(500).json({ error: 'Internal Server Error', details: err.message });
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
