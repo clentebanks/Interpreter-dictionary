83bd2d2ac0a72aa0f3f980831779f2d81b3f32cb
clentebanks <clentebanks0@gmail.com>
2024-10-26 00:12:10 -0600

    gt

--- backend-search/server.js
+++ backend-search/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // PostgreSQL client setup
 const pool = new Pool({
